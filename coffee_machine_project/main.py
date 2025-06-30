21e3c9561baa10bae74850efee4018e7d2a7a237
Diana Daraban <disu.lut@gmail.com>
2025-06-30 20:03:05 +0300

    first actions on the project

---
+++ coffee_machine_project/main.py
@@ -0,0 +1,11 @@
+from menu import MENU
+
+coffee_served = True
+
+def serve_coffee():
+    coffee_served = True
+
+# input for asking a coffee
+while coffee_served:
+    choice = input('What would you like? (espresso/latte/cappuccino):\n')
+
---
+++ menu.py
@@ -0,0 +1,25 @@
+MENU={
+    'espresso':{
+        'ingredients':{
+            'water':50,
+            'coffee':18,
+        },
+        'costs': 1.5,
+    },
+    'latte':{
+        'ingredients':{
+            'water':200,
+            'milk':150,
+            'coffee':24,
+        },
+        'costs': 2.5,
+    },
+    'cappuccino':{
+        'ingredients':{
+            'water':250,
+            'milk':100,
+            'coffee':24,
+        },
+        'costs': 3.0,
+    },
+}
