63528739ee60524db8b6af50f578f99de5d763ac
Diana Daraban <disu.lut@gmail.com>
2025-07-04 18:45:53 +0300

    added a code for a new turtle challenge - Hirst dot paint

---
+++ day_18_start/hirst_painting/main.py
@@ -0,0 +1,34 @@
+# import colorgram
+import random
+import turtle
+from turtle import Turtle
+# colors = colorgram.extract('hirst.png', 30)
+# rgb_colors = []
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     color_tuple = (r, g, b)
+#     rgb_colors.append(color_tuple)
+#
+# print(rgb_colors)
+turtle.colormode(255)
+rgb_list = [(203, 165, 107), (151, 73, 47), (52, 93, 125), (170, 153, 40), (222, 202, 136), (137, 31, 21), (132, 163, 185), (200, 91, 70), (48, 122, 88), (66, 47, 41), (14, 101, 72), (146, 178, 147), (235, 175, 165), (161, 142, 157), (108, 73, 77), (17, 86, 89), (184, 205, 173), (56, 46, 49), (38, 61, 75), (49, 65, 80), (148, 19, 22), (86, 145, 129), (184, 85, 89), (178, 191, 209), (107, 126, 152), (219, 176, 181)]
+
+tim = Turtle()
+i = 1
+tim.speed('fastest')
+while i in range(11):
+    for _ in range(11):
+        tim.pendown()
+        tim.dot(20, random.choice(rgb_list))
+        tim.penup()
+        tim.forward(50)
+    tim.setx(0)
+    tim.sety(int(tim.pos()[1]) + 50)
+    i+=1
+
+
+turtle.Screen()
+turtle.onscreenclick()
--- day_18_start/main.py
+++ day_18_start/main.py
@@ -1,9 +1,11 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape('turtle')
-timmy_the_turtle.color('red')
+# timmy_the_turtle.color('red')
+turtle.colormode(255)
 # for i in range(4):
 #     timmy_the_turtle.right(90)
 #     timmy_the_turtle.forward(100)
@@ -14,7 +16,7 @@ timmy_the_turtle.color('red')
 #     timmy_the_turtle.forward(10)
 #     timmy_the_turtle.pendown()
 
-colors = ['grey','red', 'yellow', 'blue', 'green', 'black', 'purple', 'orange']
+# colors = ['grey','red', 'yellow', 'blue', 'green', 'black', 'purple', 'orange']
 direction=[0,90,180,270]
 timmy_the_turtle.pensize(15)
 timmy_the_turtle.speed('fastest')
@@ -25,14 +27,17 @@ timmy_the_turtle.speed('fastest')
 #         timmy_the_turtle.right(360/i)
 #         timmy_the_turtle.forward(100)
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color_tuple = (r, g, b)
+    return color_tuple
 
 for _ in range(200):
-    timmy_the_turtle.color(random.choice(colors))
+    timmy_the_turtle.color(random_color())
     timmy_the_turtle.forward(30)
     timmy_the_turtle.setheading(random.choice(direction))
-
-
-
 
 
 
